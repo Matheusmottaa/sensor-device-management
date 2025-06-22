1440c15a46f8055dd6ffa155cd2148c012850a12
Matheus <contact.mfmotta@gmail.com>
2025-06-21 22:58:06 -0300

    New project files

---
+++ src/main/java/io/github/matheusmottaa/sensor/device/management/SensorDeviceManagementApplication.java
@@ -0,0 +1,13 @@
+package io.github.matheusmottaa.sensor.device.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorDeviceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorDeviceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/matheusmottaa/sensor/device/management/SensorDeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.matheusmottaa.sensor.device.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorDeviceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
